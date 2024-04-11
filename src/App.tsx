1613e30b59c84ca72a9208e02b2184645bf08693
FridaHaggstromIths <frida.haggstrom@iths.se>
2024-04-11 12:00:37 +0200

    nästan klar

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import { Link, Routes, Route } from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home'
+import About from './pages/About'
+import { useCarContext } from './context/CarContext'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const { cars } = useCarContext()
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <div className="App">
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand>
+                        <img
+                            className="logo"
+                            src="/images/0x0-Tesla_Wordmark_20_Black.png"
+                            alt="logo"
+                        />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav id="Nav.Link" className="me-auto">
+                            {cars &&
+                                cars.map((car: string, index: number) => (
+                                    <li key={index}>
+                                        <Link to={'/'}>{car}</Link>
+                                    </li>
+                                ))}
+                        </Nav>
+                        <Nav>
+                            <Link id="Nav.Link" to={'/'}>
+                                Startsidan
+                            </Link>
+                            <Link id="Nav.Link" to={'/about'}>
+                                About
+                            </Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+        </div>
+    )
 }
 
 export default App
